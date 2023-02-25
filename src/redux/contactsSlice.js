1b8d5d3293015cb136fff78c4f82fcd370b548eb
MythosOne <107143884+MythosOne@users.noreply.github.com>
2023-02-25 20:17:16 +0200

    goit-react-hw-07-phonebook-contactsSlice-refactor

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,14 +1,8 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { fetchContacts, addContact, deleteContact } from '../redux/operations';
 
-const handlePending = state => {
-  state.isLoading = true;
-};
-
-const handleRejected = (state, action) => {
-  state.isLoading = false;
-  state.error = action.payload;
-};
+const extraActions = [fetchContacts, addContact, deleteContact];
+const getActions = (type) => extraActions.map(action => action[type]);
 
 export const contactsSlice = createSlice({
   name: 'contacts',
@@ -18,36 +12,37 @@ export const contactsSlice = createSlice({
     error: null,
     filter: '',
   },
-  extraReducers: {
-    [fetchContacts.pending]: handlePending,
-    [fetchContacts.fulfilled] (state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    [fetchContacts.rejected]: handleRejected,
-
-    [addContact.pending]: handlePending,
-    [addContact.fulfilled] (state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items.push(action.payload);
-    },
-    [addContact.rejected]: handleRejected,
-
-    [deleteContact.pending]: handlePending,
-    [deleteContact.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = state.items.filter(
-        contact => contact.id !== action.payload.id);
-    },
+  extraReducers: builder => {
+    return builder
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.items = action.payload;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.items = state.items.filter(
+          contact => contact.id !== action.payload.id
+        );
+      })
+      .addMatcher(isAnyOf(...getActions('pending')), state => {
+        state.isLoading = true;
+      })
+      .addMatcher(isAnyOf(...getActions('rejected')), (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addMatcher(isAnyOf(...getActions('fulfilled')), state => {
+        state.isLoading = false;
+        state.error = null;
+      });
   },
 
   reducers: {
     setFilter(state, action) {
       state.filter = action.payload;
-    }},
+    },
+  },
 });
 
 export const { setFilter } = contactsSlice.actions;
